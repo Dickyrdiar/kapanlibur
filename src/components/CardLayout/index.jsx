063b3acd1059509f5af1f85d64fbb2d9381a6a30
dicky ardiar <diackyardiar1@gmail.com>
2023-10-03 15:40:39 +0700

    adding axios

--- src/components/CardLayout/index.jsx
+++ src/components/CardLayout/index.jsx
@@ -5,7 +5,10 @@ const CardLayout = () => {
   return (
     <div className="flex justify-center item-center mt-2">
       <div className="w-3/4 h-48 bg-white-300 shadow-lg tex-white p-4 mt-6 rounded">
-        <p>this is card</p>
+        <div className="flex justify-between pt-8 py-8">
+          <div>calendar</div>
+          <div>list</div>
+        </div>
       </div>
     </div>
   )
